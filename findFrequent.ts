fdaf9884c7397aa3f97bffc8d6c675d47970231c
Quang Pham <quangpham.245@protonmail.com>
2022-05-04 17:14:12 +0700

    Implement findFrequent

--- findFrequent.ts
+++ findFrequent.ts
@@ -1,3 +1,23 @@
-export default function findFrequent(numbers: any[]): any {
-  throw new Error("Unimplemented");
+export default function findFrequent(elements: any[]): any {
+  const frequencies = new Map<any, number>();
+
+  elements.forEach((el) => {
+    if (frequencies.has(el)) {
+      frequencies.set(el, frequencies.get(el)! + 1);
+    } else {
+      frequencies.set(el, 1);
+    }
+  });
+
+  let currentMostFrequentElement: any = undefined;
+  let currentHighestFrequency: number = 0;
+
+  frequencies.forEach((frequency: number, element: any) => {
+    if (frequency > currentHighestFrequency) {
+      currentMostFrequentElement = element;
+      currentHighestFrequency = frequency;
+    }
+  });
+
+  return currentMostFrequentElement;
 }
